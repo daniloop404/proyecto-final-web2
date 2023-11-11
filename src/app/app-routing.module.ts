400a3caab2110d68f622b9b66b9f592bcf216640
DenisChasipanta <DenisChasipanta@users.noreply.github.com>
2023-11-10 20:16:37 -0500

    home/nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductosComponent } from './screens/productos/productos.component';
 import { DetalleProductoComponent } from './components/detalle-producto/detalle-producto.component';
 import { CarritoComprasComponent } from './screens/carrito-compras/carrito-compras.component';
+import { HomeComponent } from './screens/home/home.component';
+import { NosotrosComponent } from './screens/nosotros/nosotros.component';
 
 const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'nosotros', component:NosotrosComponent},
   {path: 'productos', component:ProductosComponent},
   {path: 'detalleProducto/:id', component:DetalleProductoComponent},
   { path: 'carritocompras', component: CarritoComprasComponent },
