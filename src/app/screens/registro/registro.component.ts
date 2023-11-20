8d9192f13465fe37e7404a04ea3e45ef8f69276a
daniloop404 <daniloop404@gmail.com>
2023-11-19 20:18:47 -0500

    agregado comprobaciones

--- src/app/screens/registro/registro.component.ts
+++ src/app/screens/registro/registro.component.ts
@@ -1,3 +1,4 @@
+// registro.component.ts
 import { Component } from '@angular/core';
 import { UsuariosService } from 'src/app/services/usuarios.service';
 import { Router } from '@angular/router';
@@ -18,25 +19,57 @@ export class RegistroComponent {
     correo: ''
   };
 
+  usernameExists = false;
+  emailExists = false;
+
   // Inject the Router service in the constructor
   constructor(private usuariosService: UsuariosService, private router: Router) {}
 
   onSubmit() {
-    this.usuariosService.postUsuario(this.nuevoUsuario).subscribe(
-      (response) => {
-        console.log('Usuario registrado:', response);
+    // Check if the form is valid and if both username and email are unique
+    if (!this.usernameExists && !this.emailExists) {
+      this.usuariosService.postUsuario(this.nuevoUsuario).subscribe(
+        (response) => {
+          console.log('Usuario registrado:', response);
 
-        // Show success message to the user
-        alert('Usuario registrado exitosamente, ahora puedes iniciar sesion');
+          // Show success message to the user
+          alert('Usuario registrado exitosamente, ahora puedes iniciar sesión');
 
-        // Optionally, you can reset the form here
-        // this.nuevoUsuario = { nombreUsuario: '', clave: '', rol: '', nombre: '', direccion: '', telefono: '', correo: '' };
+          // Optionally, you can reset the form here
+          // this.nuevoUsuario = { nombreUsuario: '', clave: '', rol: '', nombre: '', direccion: '', telefono: '', correo: '' };
 
-        // Redirect to the login page
-        this.router.navigate(['/login']);
+          // Redirect to the login page
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error al registrar usuario:', error);
+        }
+      );
+    }
+  }
+
+  checkUsername() {
+    console.log('Checking username:', this.nuevoUsuario.nombreUsuario);
+    this.usuariosService.checkUsernameExists(this.nuevoUsuario.nombreUsuario).subscribe(
+      (exists) => {
+        console.log('Username exists:', exists);
+        this.usernameExists = exists;
       },
       (error) => {
-        console.error('Error al registrar usuario:', error);
+        console.error('Error checking username:', error);
+      }
+    );
+  }
+  
+  checkEmail() {
+    console.log('Checking email:', this.nuevoUsuario.correo);
+    this.usuariosService.checkEmailExists(this.nuevoUsuario.correo).subscribe(
+      (exists) => {
+        console.log('Email exists:', exists);
+        this.emailExists = exists;
+      },
+      (error) => {
+        console.error('Error checking email:', error);
       }
     );
   }
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -25,5 +27,18 @@ export class UsuariosService {
   putUsuario(key: string, usuario: any): Observable<any> {
     const url = `${this.API_USUARIOS}/${key}.json`;
     return this.http.put(url, usuario);
+  }
+  public checkUsernameExists(username: string): Observable<boolean> {
+    const url = `${this.API_USUARIOS}.json?orderBy="nombreUsuario"&equalTo="${username}"`;
+    return this.http.get<any>(url).pipe(
+      map(response => response && Object.values(response).length > 0)
+    );
+  }
+
+  public checkEmailExists(email: string): Observable<boolean> {
+    const url = `${this.API_USUARIOS}.json?orderBy="correo"&equalTo="${email}"`;
+    return this.http.get<any>(url).pipe(
+      map(response => response && Object.values(response).length > 0)
+    );
   }
 }
